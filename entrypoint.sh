494c503741e9a38f24cb6ae2dfbef883f5a54168
Nicolas Boltz <boltz.nicolas@gmx.de>
2023-01-05 11:25:59 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,7 +23,7 @@ sftp -b $TEMP_SFTP_FILE -P $3 -o ConnectTimeout=5 -o StrictHostKeyChecking=no -i
 
 if [ $7 ]
 then
-  ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 ln -sf $6 $8/releases/latest
+  ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 ln -sf ./6.0.0 $8/releases/latest
 fi
 
 echo 'Deployment success'
