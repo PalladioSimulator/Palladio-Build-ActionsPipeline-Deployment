12b0213875087a07e2411690450b9c4f34f9fa03
Nicolas Boltz <boltz.nicolas@gmx.de>
2023-01-23 13:35:58 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,17 +12,20 @@ chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
 
 echo 'Start Deployment'
 
-# Create deployment folder if it does not exist
+echo 'Create deployment folder if it does not exist'
 ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 mkdir -p $6
 
-# Remove potential existig files in deployment folder
+echo 'Remove potential existig files in deployment folder'
 ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 rm -rf $6/*
 
 # Copy new files to folder
+echo 'Start copying files...'
 sftp -b $TEMP_SFTP_FILE -P $3 -o ConnectTimeout=5 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2
+echo 'Copying finished'
 
 if [ $7 -ne '0.0.0' ]
 then
+  echo 'Write latest release link'
   ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 ln -sf '$6' $8/releases/latest
 fi
 
